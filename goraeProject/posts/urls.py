aede510e1ee05d86917f8640fb4ac0fec1d0f96c
chaechaeyeon <00dreamadream@kakao.com>
2023-08-03 17:12:13 +0900

    수정 삭제 권한 수정

--- goraeProject/posts/urls.py
+++ goraeProject/posts/urls.py
@@ -1,10 +1,7 @@
 from rest_framework.routers import SimpleRouter, DefaultRouter
 from django.urls import path, include
-from .views import MessageViewSet, ReceivedMessageViewSet
+from .views import  ReceivedMessageViewSet
 
-#전체 메세지 확인용
-message_router = DefaultRouter(trailing_slash=True)
-message_router.register('messages', MessageViewSet, basename='message')
 
 #받은 메세지 
 received_router = DefaultRouter(trailing_slash=True)
@@ -13,6 +10,6 @@ received_router.register(r'received/(?P<user_id>\d+)', ReceivedMessageViewSet, b
 
 
 urlpatterns = [
-    path('', include(message_router.urls)),
+    
     path('',include(received_router.urls)),
 ]
--- goraeProject/posts/views.py
+++ goraeProject/posts/views.py
@@ -10,45 +10,12 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly ,IsAuthenticate
 from .permissions import IsAuthorOrReadOnly
 
 
-#전체확인용
-class MessageViewSet(ModelViewSet):
-    # #authentication 추가 
-    # authentication_classes = [BasicAuthentication, SessionAuthentication]
-
-    #Permission 기능 : isAuthenticated(읽기권한 x) /IsAuthenticatedOrReadOnly
-    permission_classes= [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-
-   
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-
-    #게시글 생성 
-    def perform_create(self, serializer) :
-        serializer.save(writer=self.request.user) 
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        
-        self.perform_create(serializer)
-        
-        #칭찬 보내는 사람 포인트부여
-        serializer.instance.writer.userinfo.points += 10
-        serializer.instance.writer.userinfo.save()
-        #칭찬 받는 사람 카운트 증가 
-        receiver = serializer.validated_data.get('receiver') #받은 사람 정보 가져오기
-        receiver.userinfo.count += 1
-        receiver.userinfo.save()
-        
-
-        headers = self.get_success_headers(serializer.data) #성공시 헤더로 전달할 값들을 headers에 담기
-        return Response(serializer.data,headers=headers) #response로 data, headers 전송 
 
 #개개인 메세지 전달
 class ReceivedMessageViewSet(ModelViewSet):
     
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes= [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         user_id = self.kwargs['user_id']
